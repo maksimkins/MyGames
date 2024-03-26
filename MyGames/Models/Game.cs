06ce3bb2b219145751e4ae56973d7751055c7658
maksimkins <maks.aleshkov.04@gmail.com>
2024-03-26 19:35:48 +0400

    Add Models

---
+++ MyGames/Models/Game.cs
@@ -0,0 +1,5 @@
+public class Game {
+    public int? Id { get; set; }
+    public string? Name {set; get;}
+    public decimal? Price{set; get;}
+}
---
+++ MyGames/Models/User.cs
@@ -0,0 +1,7 @@
+public class User {
+
+    public int? Id { get; set; }
+    public string? Name {set; get;}
+    public string? Surname{set; get;}
+    public DateTime? Birthdate {set; get;}
+}

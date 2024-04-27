d12460791f8a0f50c31681f4bc1c8cbdc3cbf0b5
maksimkins <maks.aleshkov.04@gmail.com>
2024-04-27 23:16:31 +0400

    Change GameController and its Index

--- src/MyGames/Controllers/GameController.cs
+++ src/MyGames/Controllers/GameController.cs
@@ -2,52 +2,24 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MyGames.Models;
 using System.Text.Json;
+using MyGames.Services.Base;
 
 namespace MyGames.Controllers;
 
 public class GameController : Controller {
 
-    private readonly ILogger<HomeController> _logger;
+    private readonly IGameService service;
 
-    public GameController(ILogger<HomeController> logger)
+    public GameController(IGameService service)
     {
-        _logger = logger;
+        this.service = service;
     }
 
     
     [HttpGet("[controller]")]
-    public IActionResult Index ()
+    public async Task<IActionResult> Index ()
     {
-        // System.Console.WriteLine("index");
-        return View();
+        var games = await service.AllGamesAsync();
+        return View(games);
     }
-
-    
-    // [Route("[controller]")]
-    // [HttpPost]
-    // public async Task<IActionResult> CreateGame(GameDto newGame) {
-
-    //     if(newGame.Name is null || string.IsNullOrWhiteSpace(newGame.Name) || string.IsNullOrEmpty(newGame.Name) || newGame.Price is null) {
-    //         return BadRequest();
-    //     }
- 
-    //     var gamesJSon = await System.IO.File.ReadAllTextAsync("../../src/MyGames/json/Games.json");
-
-    //     var games = JsonSerializer.Deserialize<List<Game>>(gamesJSon, new JsonSerializerOptions {
-    //         PropertyNameCaseInsensitive = true,
-    //     });
-
-    //     games?.Add(new Game() {
-    //         Name = newGame.Name,
-    //         Price = newGame.Price,
-    //     });
-
-    //     var resultGamesJson = JsonSerializer.Serialize(games, new JsonSerializerOptions {
-    //         PropertyNameCaseInsensitive = true,
-    //     });
-
-    //     await System.IO.File.WriteAllTextAsync("../../src/MyGames/json/Games.json", resultGamesJson);
-
-    //     return base.RedirectToAction(actionName: "Index");
-    // }
 }

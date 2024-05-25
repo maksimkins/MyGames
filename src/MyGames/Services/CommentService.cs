64fef8b89ca66a7634c1494a2ebe681ef73826bc
maksimkins <maks.aleshkov.04@gmail.com>
2024-05-25 23:40:54 +0400

    Change Services dut to Models

--- src/MyGames/Services/CommentService.cs
+++ src/MyGames/Services/CommentService.cs
@@ -27,7 +27,7 @@ namespace MyGames.Services
 
         public async Task CreateCommentAsync(Comment comment)
         {
-            if (comment == null || comment.GameId == null || comment.Title == null || comment.Text == null)
+            if (comment == null || comment.GameId == null || comment.Title == null || comment.Text == null || comment.CreationDate == null || comment.Rate == null)
             {
                 throw new ArgumentNullException(nameof(comment));
             }

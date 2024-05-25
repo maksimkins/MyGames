b60721761981164e41de79d39c76b68c3f3c140a
maksimkins <maks.aleshkov.04@gmail.com>
2024-05-25 23:19:07 +0400

    Renew Sql Tables

--- src/MyGames/sql/creates.sql
+++ src/MyGames/sql/creates.sql
@@ -1,34 +1,67 @@
- create database MyGames;
+create database MyGames;
 
-  use MyGames;
+use MyGamesDb;
 
- create table Games(
-     Id INT PRIMARY KEY IDENTITY(1, 1),
-     Name NVARCHAR(60),
-	 Description nvarchar(300) null,
-	 Price money null
+create table GamesSeries(
+	Id int primary key identity(1, 1),
+	Name nvarchar(max) not null
+);
+
+create table Developers(
+	Id int primary key identity(1, 1),
+	Name nvarchar(max) not null
+);
+
+create table Genres(
+	Id int primary key identity(1, 1),
+	Name nvarchar(max) not null
+);
+
+create table Games(
+     Id int primary key identity(1, 1),
+	 SerieId int foreign key references GamesSeries(Id) null,
+     Name nvarchar(max) not null,
+	 Description nvarchar(max) null,
+	 Price money not null,
+	 CreationDate datetime2 null,
+	 DeveloperId int foreign key references GamesSeries(Id) null,
+	 Rate float null,
+	 ForAdultsOnly bit null,
+	 PictureUrl nvarchar(max) null
+ );
+
+ create table Genres_Games(
+	Id int primary key identity(1, 1),
+	GameId int foreign key references Games(Id) on delete cascade not null,
+	GenreId int foreign key references Genres(Id) on delete cascade not null,
+ );
+
+ create table Addons(
+	Id int primary key identity(1, 1),
+	Name nvarchar(max) not null,
+	Description nvarchar(max) null,
+	GameId int foreign key references Games(Id) on delete cascade not null,
  );
 
  create table Comments(
-     Id INT PRIMARY KEY IDENTITY(1, 1),
-     Title NVARCHAR(60),
+     Id int primary key identity(1, 1),
+     Title nvarchar(60),
 	 Text nvarchar(300),
-	 GameId int foreign key references Games(Id)
+	 GameId int foreign key references Games(Id) on delete cascade not null,
+	 Rate float not null,
+	 CreationDate datetime2 not null,
+	 ChangeDate datetime2 null,
  );
 
- insert into Games(Name, Description, Price)
- values
- ('Battlefront 2', 'coop shooter from StarWars universe part 2', 50),
- ('Battlefront 1', 'coop shooter from StarWars universe', 30),
- ('Terraria', 'sandbox from independent developers', 15)
-
- insert into Comments(GameId, Title, Text)
- values
- (1, 'awesome', 'awesome game!!'),
- (2, 'nice', 'nice game!!'),
- (2, 'bad', 'bad game!!')
-
-
+ create table Users(
+	Id int primary key identity(1, 1),
+	Login varchar(60) not null,
+	Password varchar(60) not null,
+	Email varchar(320) not null,
+	UserName nvarchar(60) not null,
+	Balance money null,
+	BirthDate datetime2 not null
+ );
 
  create table Logs(
 	Id int primary key identity(1, 1),
@@ -39,10 +72,16 @@
 	EndDate datetime2 not null,
 	StatusCode int not null,
 	HttpMethod nvarchar(20)
- )
+ );
 
- --use MyGames;
+ insert into Games(Name, Description, Price)
+ values
+ ('Battlefront 2', 'coop shooter from StarWars universe part 2', 50),
+ ('Battlefront 1', 'coop shooter from StarWars universe', 30),
+ ('Terraria', 'sandbox from independent developers', 15)
 
- --select * 
- --from Logs;
-
+ insert into Comments(GameId, Title, Text, Rate, CreationDate)
+ values
+ (1, 'awesome', 'awesome game!!', 4.5, '14-6-2022'),
+ (2, 'nice', 'nice game!!', 4, '13-5-2022'),
+ (2, 'bad', 'bad game!!', 2.5, '14-5-2022')

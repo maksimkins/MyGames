14c9d87e5b6ee43bc80cb4f8bb4a9bfba947a067
maksimkins <maks.aleshkov.04@gmail.com>
2024-05-25 23:30:06 +0400

    Change Models

--- src/MyGames/Models/Comment.cs
+++ src/MyGames/Models/Comment.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MyGames.Models
-{
-    public class Comment
-    {
+namespace MyGames.Models;
+
+    public class Comment {
         public int? Id { get; set; }
         public int? GameId { get; set; }
         public string? Title {set; get;}
         public string? Text {set; get;}
+        public DateTime? CreationDate{set; get;}
+        public DateTime? ChangeDate{set; get;}
+        public float? Rate{set; get;}
     }
-}
+    
--- src/MyGames/Models/Game.cs
+++ src/MyGames/Models/Game.cs
@@ -5,7 +5,13 @@ namespace MyGames.Models;
 
 public class Game {
     public int? Id { get; set; }
+    public int? SerieId { get; set; }
+    public int? DeveloperId { get; set; }
     public string? Name {set; get;}
     public string? Description {set; get;}
     public decimal? Price{set; get;}
+    public DateTime? CreationDate{set; get;}
+    public float? Rate{set; get;}
+    public bool? ForAdultsOnly{set; get;}
+    public string? PictureUrl{set; get;}
 }

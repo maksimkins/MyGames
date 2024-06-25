cec89e654fa223ef702e9177f046c13ef343df7a
maksimkins <maks.aleshkov.04@gmail.com>
2024-06-25 17:01:16 +0400

    Add UnitTests

---
+++ test/MyGames.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using MyGames.Services;
+using Moq;
+using MyGames.Repositories;
+using MyGames.Models;
+using MyGames.Repositories.Base;
+
+namespace MyGames.Tests.Services;
+
+public class UserServiceTests
+{
+    [Fact]
+    public async Task GetUserAsync_UserFound_DoesntThrowException()
+    {
+        Mock<IUserRepository> userRepositoryMock = new Mock<IUserRepository>();
+
+        int userId = 1;
+        userRepositoryMock.Setup((repo) => repo.GetByIdAsync(userId))
+            .ReturnsAsync(new User() {
+                Username = "testik12",
+                Id = userId,
+            });
+
+        var userService = new UserService(
+            repository: userRepositoryMock.Object);
+
+        await userService.GetByIdAsync(userId);
+    }
+
+    [Fact]
+    public async Task GetUserAsync_NotProperId_ThrowsArgumentNullException()
+    {
+        Mock<IUserRepository> userRepositoryMock = new Mock<IUserRepository>();
+
+        int userId = -1;
+        userRepositoryMock.Setup((repo) => repo.GetByIdAsync(userId))
+            .ReturnsAsync(value: null);
+
+        var userService = new UserService(
+            repository: userRepositoryMock.Object);
+        
+        await Assert.ThrowsAsync<ArgumentNullException>(async () => await userService.GetByIdAsync(userId));
+    }
+}
